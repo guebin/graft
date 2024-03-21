e5f7aba84ad29a030c538d96dfb79dd33d3c0d20
miruetoto <miruetoto@gmail.com>
2024-03-21 17:46:02 +0900

    .

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='graft',
+        version='0.0.1',
+        description='graft',
+        url='https://github.com/guebin/graft',
+        author='Guebin Choi',
+        author_email='',
+        license='',
+        packages=find_packages(),
+        install_requires=['pandas']
+)
+
+
