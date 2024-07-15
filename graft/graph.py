8e41d3d2c1108266fcd9a69166a25a62a5e8e9b5
miruetoto <miruetoto@gmail.com>
2024-07-15 17:38:05 +0900

    .

--- graft/graph.py
+++ graft/graph.py
@@ -99,7 +99,7 @@ def plot_undirected_weighted(
     v_size, gt_graph = map_vertex_size(gt_graph, vertices, node_size)    
 
     # 6. Set links.
-    e_weight, e_pen_width, e_arrow_size, gt_graph = set_links_weighted(gt_graph,vertices,links,weights,edge_weight_text_format,edge_weight_width_scale,arrow_size_scale)
+    e_weight, e_pen_width, gt_graph = set_links_weighted(gt_graph,vertices,links,weights,edge_weight_text_format,edge_weight_width_scale)
 
     # 7. Set draw_options.
     draw_options.setdefault('output_size', (150 + num_nodes, 150 + num_nodes)) # Set default output size if not provided in draw_options
@@ -211,7 +211,7 @@ def plot_directed_weighted(
     v_size, gt_graph = map_vertex_size(gt_graph, vertices, node_size)    
 
     # 6. Set links.
-    e_weight, e_pen_width, e_arrow_size, gt_graph = set_links_weighted(gt_graph,vertices,links,weights,edge_weight_text_format,edge_weight_width_scale,arrow_size_scale)
+    e_weight, e_pen_width, gt_graph = set_links_weighted(gt_graph,vertices,links,weights,edge_weight_text_format,edge_weight_width_scale)
     # 7. Set draw_options.
     draw_options.setdefault('output_size', (150 + num_nodes, 150 + num_nodes)) # Set default output size if not provided in draw_options
     if node_color is not None:
@@ -222,7 +222,6 @@ def plot_directed_weighted(
         draw_options['edge_text'] = e_weight  # Set edge text property
     if edge_weight_width: 
         draw_options['edge_pen_width'] = e_pen_width  # Use edge weight to adjust edge pen width
-        draw_options['arrow_size'] = e_arrow_size  # Use edge weight to adjust edge pen width
 
     # 8. Perform graph layout using sfdf_layout and draw the graph using graph_draw.
     pos = gt.sfdp_layout(gt_graph, **layout_options)
--- graft/graph_links.py
+++ graft/graph_links.py
@@ -9,13 +9,11 @@ def set_links_unweighted(gt_graph,links):
     gt_graph.add_edge_list(links.t().tolist())
     return gt_graph
 
-def set_links_weighted(gt_graph,vertices,links,weights,edge_weight_text_format,edge_weight_width_scale,arrow_size_scale):
+def set_links_weighted(gt_graph,vertices,links,weights,edge_weight_text_format,edge_weight_width_scale):
     e_weight = gt_graph.new_edge_property("string")
     e_pen_width = gt_graph.new_edge_property("double")
-    e_arrow_size = gt_graph.new_edge_property("double")
     for idx, (start, end) in enumerate(links.t().tolist()):
         e = gt_graph.add_edge(vertices[start], vertices[end])
         e_weight[e] = format(weights[idx].item(),edge_weight_text_format)
         e_pen_width[e] = (weights/weights.max())[idx].item() * edge_weight_width_scale
-        e_arrow_size[e] = (weights / weights.max())[idx].item() * arrow_size_scale
-    return e_weight, e_pen_width, e_arrow_size, gt_graph
+    return e_weight, e_pen_width, gt_graph
