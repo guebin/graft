172f2c5b455f0c7968aa331ffbba8cf25e4f187a
miruetoto <miruetoto@gmail.com>
2024-07-15 15:29:28 +0900

    .

--- graft/graph.py
+++ graft/graph.py
@@ -195,24 +195,24 @@ def plot_directed_weighted(
     layout_options = layout_options or {}
     draw_options = draw_options or {}
 
-    # 2. Extract graph components.
-    links, weights, num_nodes, x, y = extract_graph_components_dw(graph)
+    # 2. extract_graph_components.
+    links, weights, num_nodes, x, y = extract_graph_components_udw(graph)
 
     # 3. Create a graph_tool graph.
-    gt_graph = gt.Graph(directed=True)  # Directed graph
+    gt_graph = gt.Graph(directed=True)  # Undirected graph
 
     # 4. Set nodes.
     vertices, v_text_prop, gt_graph = set_nodes(gt_graph, num_nodes, node_names)
 
     # 5. Map colors and sizes.
     v_color, gt_graph = map_vertex_color(gt_graph, vertices, node_color)
-    v_size, gt_graph = map_vertex_size(gt_graph, vertices, node_size)
+    v_size, gt_graph = map_vertex_size(gt_graph, vertices, node_size)    
 
     # 6. Set links.
-    e_weight, e_pen_width, gt_graph = set_links_weighted(gt_graph, vertices, links, weights, edge_weight_text_format, edge_weight_width_scale)
+    e_weight, e_pen_width, gt_graph = set_links_weighted(gt_graph,vertices,links,weights,edge_weight_text_format,edge_weight_width_scale)
 
     # 7. Set draw_options.
-    draw_options.setdefault('output_size', (150 + num_nodes, 150 + num_nodes))  # Set default output size if not provided in draw_options
+    draw_options.setdefault('output_size', (150 + num_nodes, 150 + num_nodes)) # Set default output size if not provided in draw_options
     if node_color is not None:
         draw_options['vertex_fill_color'] = v_color  # Set the vertex color based on y
     if node_size is not None:
@@ -222,6 +222,6 @@ def plot_directed_weighted(
     if edge_weight_width: 
         draw_options['edge_pen_width'] = e_pen_width  # Use edge weight to adjust edge pen width
 
-    # 8. Perform graph layout using sfdp_layout and draw the graph using graph_draw.
+    # 8. Perform graph layout using sfdf_layout and draw the graph using graph_draw.
     pos = gt.sfdp_layout(gt_graph, **layout_options)
     gt.graph_draw(gt_graph, pos=pos, vertex_text=v_text_prop, **draw_options)
