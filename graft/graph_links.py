75b8f34919f8062e66da115ef55b3fea6f4e31e4
miruetoto <miruetoto@gmail.com>
2024-07-18 06:51:25 +0900

    .

--- graft/graph_links.py
+++ graft/graph_links.py
@@ -73,6 +73,8 @@ def map_edge_pen_widths(
 
     for idx, (start, end) in enumerate(links.t().tolist()):
         e = gt_graph.add_edge(vertices[start], vertices[end])
-        e_pen_width[e]# = (weights / weights.max())[idx].item() * edge_weight_width_scale
+        print(weights)
+        #print(weights)
+        #e_pen_width[e] = (weights / weights.max())[idx].item() * edge_weight_width_scale
     draw_options['edge_pen_width'] = e_pen_width        
     return gt_graph, draw_options
