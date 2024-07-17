6594b60e6d9d6e0a0aded9edffbcc8cc93d0554d
miruetoto <miruetoto@gmail.com>
2024-07-18 06:07:05 +0900

    .

--- graft/graph_nodes.py
+++ graft/graph_nodes.py
@@ -14,7 +14,7 @@ def map_vertex_names(gt_graph, draw_options, node_names):
         v_text_prop (graph_tool.VertexPropertyMap): Vertex property map with node names.
         gt_graph (graph_tool.Graph): Modified graph with vertex names.
     """
-    vertices = [l for l in gt_graph.vertices()]
+    vertices = list(gt_graph.vertices())
     if node_names is None:
         return gt_graph, draw_options
     else: 
@@ -36,7 +36,7 @@ def map_vertex_color(gt_graph, draw_options, node_colors, alpha=1.0):
     Returns:
         v_color (graph_tool.VertexPropertyMap): Vertex property map with colors.
     """
-    vertices = gt_graph.vertices()
+    vertices = list(gt_graph.vertices())
     if node_colors is None:
         return gt_graph, draw_options
     else: 
@@ -51,7 +51,7 @@ def map_vertex_color(gt_graph, draw_options, node_colors, alpha=1.0):
                 normalized_value = (value - y_min) / (y_max - y_min)  # Normalize between [0, 1]
                 rgba = list(colormap(normalized_value))  # Get RGBA color
                 rgba[-1] = alpha  # Set the alpha value
-                v_color[vertices[idx]] = rgba
+                v_color[idx] = rgba
         else: # Categorical or discrete values
             colors_dict = {
                 1: ['#F8766D'],
@@ -78,7 +78,7 @@ def map_vertex_color(gt_graph, draw_options, node_colors, alpha=1.0):
             y_to_color = {int(val): ggplot_colors_rgb[val] for val in unique_y}
             for idx, value in enumerate(y):
                 rgb = y_to_color[int(value)]
-                v_color[vertices[idx]] = rgb
+                v_color[idx] = rgb
         draw_options['vertex_fill_color'] = v_color
         return gt_graph, draw_options
 
@@ -94,7 +94,7 @@ def map_vertex_size(gt_graph, draw_options, node_sizes):
     Returns:
         v_size (graph_tool.VertexPropertyMap): Vertex property map with sizes.
     """
-    vertices = gt_graph.vertices()
+    vertices = list(gt_graph.vertices())
     if node_sizes is None:
         return gt_graph, draw_options
     else: 
@@ -106,6 +106,6 @@ def map_vertex_size(gt_graph, draw_options, node_sizes):
         for idx, value in enumerate(y):
             normalized_value = (value - y_min) / (y_max - y_min)  # Normalize between [0, 1]
             size = min_size + normalized_value * (max_size - min_size)  # Map to size range
-            v_size[vertices[idx]] = size
+            v_size[idx] = size
         draw_options['vertex_size'] = v_size  
         return gt_graph, draw_options
